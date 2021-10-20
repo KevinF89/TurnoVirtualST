a2f77241cd9cbd1618b253521481b7d7881a5151
Kevin Forero <kforero@dondoctor.com>
2021-10-19 20:13:31 -0500

    cambio ruta 5

--- src/app/Modulos/shared/ventana/ventana.component.ts
+++ src/app/Modulos/shared/ventana/ventana.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Mensaje, tipoMensaje, respuestaMensaje } from '../interfaces/mensaje'
+import { Mensaje, tipoMensaje, respuestaMensaje } from 'src/app/Interfaces//mensaje'
 
 @Component({
   selector: 'app-ventana',
